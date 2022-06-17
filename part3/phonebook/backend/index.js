2a9dbc94d0e8cff1828231fd40560b0f8d7475a9
David Hanny <david.hanny@gmx.at>
2022-06-17 21:07:14 +0200

    exercises 3.7-3.8

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+// custom token to print request body
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
 app.use(express.json())
+// app.use(morgan('tiny'))  // predefined style
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))  // custom style with json body
 
 const PORT = 3001
 
