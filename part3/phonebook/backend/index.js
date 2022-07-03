97c1cb52652b4e0909d8dc0272d490f6e139fa81
David Hanny <david.hanny@gmx.at>
2022-07-03 12:26:45 +0200

    exercises 3.15-3.18

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -17,46 +17,63 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 const PORT = process.env.PORT || 3001
 
 // get a list of persons
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(result => {
       response.json(result)
     })
+    .catch(error => next(error))
   })
 
 // get information about the person list
-app.get('/info', (request, response) => {
-    message = `<p>Phonebook has info for ${persons.length} people.</p>`
-    message += `<p>${new Date(Date.now())}</p>`
-    response.send(message)
+app.get('/info', (request, response, next) => {
+    Person.count({})
+        .then(count => {
+            message = `<p>Phonebook has info for ${count} people.</p>`
+            message += `<p>${new Date(Date.now())}</p>`
+            response.send(message)
+        })
+        .catch(error => next(error))
 })
 
 // get information about a particular person
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {    
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+            
+        })
+        .catch(error => next(error))
 })
 
 // delete a person
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    if(person) {
-        persons = persons.filter(person => person.id !== id)
-        response.status(204).end()
-    } else {
-        response.status(404).end()
+// update a person
+app.put('/api/persons/:id', (request, response, next) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number
     }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 // add a person to the database
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     // const names = persons.map(person => person.name) 
 
@@ -73,23 +90,38 @@ app.post('/api/persons', (request, response) => {
     }
 
     // name already exists
-    /*
-    else if(names.includes(body.name)) {
+    /* else if(Person.find({"name": body.name})) {
         return response.status(400).json({ 
             error: 'name must be unique' 
           })
-    }*/
+    } */
 
     // everything works 
-      const person = new Person({
-          id: id,
-          name: body.name,
-          number: body.number
-      })
-      person.save().then(savedPerson => {
-        response.json(savedPerson)
-      })
+    const person = new Person({
+        id: id,
+        name: body.name,
+        number: body.number
+    })
+    person.save().then(savedPerson => {
+    response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
+
+
+// define the error handler
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+  
+  // this has to be the last loaded middleware.
+app.use(errorHandler)
 
 // start the server
 app.listen(PORT, () => {
