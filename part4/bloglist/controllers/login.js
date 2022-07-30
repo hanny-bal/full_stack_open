cc2610f5bc0e6702aef4198a46987ad61809bbcb
David Hanny <david.hanny@gmx.at>
2022-07-30 21:52:12 +0200

    exercise 4.18

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -22,5 +22,15 @@ loginRouter.post('/', async (request, response) => {
         username: user.username,
         id: user._id
     }
+    const token = jwt.sign(
+        userForToken,
+        process.env.SECRET,
+        { expiresIn: 60*60 }
+    )
 
+    response
+        .status(200)
+        .send({ token, username: user.username, name: user.name })
 })
+
+module.exports = loginRouter
