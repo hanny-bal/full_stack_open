25f5721b7efa4e3d83d1b8e758085bc9402746f2
hanny-bal <david.hanny@gmx.at>
2023-02-05 10:36:05 +0100

    exercise 5.7

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -100,7 +100,7 @@ const App = () => {
 
   // blog form
   const blogForm = () => (
-    <Togglable buttonLabel = 'new note' ref={blogFormRef}>
+    <Togglable buttonLabel='new note' ref={blogFormRef} cancelLabel='cancel' buttonBeforeContent={false}>
       <BlogForm setErrorMessage={setErrorMessage} setSuccessMessage={setSuccessMessage} 
           blogs={blogs} setBlogs={setBlogs} blogFormRef={blogFormRef}/>
     </Togglable>
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from "./Togglable"
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const hideWhenDetailsVisible = { display: showDetails ? 'none' : 'inline' }
+  const showWhenDetailsVisible = { display: showDetails ? 'inline' : 'none' }
+  
+  const toggleDetailVisibility = () => {
+    setShowDetails(!showDetails)
+}
+
+  return(
+    <div style={{border: 'solid', margin: 10, padding: 10}}>
+      {blog.title} {blog.author}
+
+      <div style={hideWhenDetailsVisible}>
+        <button style={{ marginLeft: 5 }} onClick={toggleDetailVisibility}>view</button>
+      </div>
+      <div style={showWhenDetailsVisible}>
+        <button style={{ marginLeft: 5 }} onClick={toggleDetailVisibility}>hide</button>
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} 
+            <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
